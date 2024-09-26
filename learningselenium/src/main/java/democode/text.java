7948540d5f4b445d13719f6fc059e7c9ade9fc57
madhu <rmadhumitha21199@gmail.com>
2024-09-26 20:56:23 +0530

    x

--- learningselenium/src/main/java/democode/text.java
+++ learningselenium/src/main/java/democode/text.java
@@ -8,8 +8,6 @@ public class text {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello");
-		
 
 		List<String> list = new ArrayList<String>();
 		list.add("asdf");
